43c8c9e9843bb5803a01d4a98b656f1099081365
Tobiasz Sroka <tobiasz731@gmail.com>
2026-06-11 00:16:36 +0200

    Utworzenie projektu ASP.NET Core MVC (.NET 10) #US1

---
+++ ClinicManager/ClinicManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClinicManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
